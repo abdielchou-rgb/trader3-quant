6d08c75caaa1f072a54a8065aeb636510ff4d664
abdielchou-rgb <abdielchou@gmail.com>
2026-09-06 18:33:22 +0800

    chore: remove stray debug scripts accidentally committed

--- check_backup_final.py
+++
@@ -1,4 +0,0 @@
-import sys
-sys.path.insert(0, r'D:\Claude\projects\3号交易员')
-from trader3.v2.disclosure_sync import _BACKUP
-print('_BACKUP:', repr(_BACKUP))
--- debug_cols.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d D:\Claude\projects\3号交易员
-python -c "import sys; sys.path.insert(0, '.'); from tests.test_fix_disclosure_cninfo import _cninfo_df, _CNINFO_COL_MAP; import pandas as pd; df = _cninfo_df(); print('map:', _CNINFO_COL_MAP); print('cols:', df.columns.tolist()); print(df)"
